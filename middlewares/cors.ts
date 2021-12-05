04003aa8207f5855b954101ac3610b142bf3cf00
Prabhat Singh <Prabatwork3939@gmail.com>
2021-12-05 16:55:45 +0530

    WIP: Testing

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 module.exports = ( req: Request, res: Response, next: NextFunction) => {
 
-    console.log('checking cors middleware' );
+    console.log('checking cors middleware', req );
     res.setHeader( 'Access-Control-Allow-Origin', '*' );
     res.setHeader( 'Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE' );
     res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type, Authorization' );
