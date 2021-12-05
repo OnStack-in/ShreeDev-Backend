f8e984e78a557a1fe70fba72edb194f40d70d1c2
Prabhat-git-99 <prabatwork3939@gmail.com>
2021-12-05 23:31:06 +0530

    WIP: CORS Fixes

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -4,6 +4,7 @@ module.exports = ( req: Request, res: Response, next: NextFunction) => {
 
     console.log('checking cors middleware', req );
     res.setHeader( 'Access-Control-Allow-Origin', '*' );
+    res.header('Access-Control-Allow-Origin', '*');
     res.setHeader( 'Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE' );
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,Authorization,content-type,application/json');
     next( );
